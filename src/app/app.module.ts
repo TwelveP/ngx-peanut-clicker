3a0bacabb0b50b48c1577e4bcf4e1a97785ec778
12p! <got12g@gmail.com>
2023-10-03 11:37:33 -0300

    refactor(move): `StatusPanelComponent`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { MainScreenComponent } from './main-screen/main-screen.component';
 import { TaskQueueComponent } from './main-screen/task-queue/task-queue.component';
 import { SharedModule } from './shared/shared.module';
+import { StatusPanelComponent } from './main-screen/status-panel/status-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainScreenComponent,
-    TaskQueueComponent
+    TaskQueueComponent,
+    StatusPanelComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrustedResourceUrlPipe } from './pipes/trusted-resource-url/trusted-resource-url.pipe';
 import { TaskQueueItemComponent } from './task-queue-item/task-queue-item.component';
-import { StatusPanelComponent } from './status-panel/status-panel.component';
 import { MoneyEarntComponent } from './effects/money-earnt/money-earnt.component';
 
 @NgModule({
   declarations: [
     TrustedResourceUrlPipe,
     TaskQueueItemComponent,
-    StatusPanelComponent,
     MoneyEarntComponent
   ],
   imports: [
@@ -18,7 +16,6 @@ import { MoneyEarntComponent } from './effects/money-earnt/money-earnt.component
   exports: [
     TrustedResourceUrlPipe,
     TaskQueueItemComponent,
-    StatusPanelComponent,
     MoneyEarntComponent
   ]
 })
