a6d09955edf9b245f5166095a48177fd8de7f97d
12p! <got12g@gmail.com>
2023-10-03 11:37:33 -0300

    refactor(class): `ProgressBarComponent` fields initialization

--- src/app/shared/progress-bar/progress-bar.component.ts
+++ src/app/shared/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { filter, startWith, switchMap, takeWhile } from 'rxjs/operators';
 import { TaskQueueService } from 'src/app/task-queue.service';
@@ -8,16 +8,14 @@ import { Task } from 'src/domain/tasks';
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html'
 })
-export class ProgressBarComponent implements OnInit {
+export class ProgressBarComponent {
   @Input() task?: Task;
 
   progress$?: Observable<number>;
 
   constructor(
     private readonly taskQueueService: TaskQueueService
-  ) { }
-
-  ngOnInit(): void {
+  ) {
     this.progress$ = this.taskQueueService.activeTask$.pipe(
       filter(() => this.task?.state === 'active'),
       switchMap(() => this.taskQueueService.currentTaskProgressPercent$.pipe(
