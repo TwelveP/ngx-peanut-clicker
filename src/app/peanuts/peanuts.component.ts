c6132aa4b023563e91377f7c97b210905d95fe16
12p! <got12g@gmail.com>
2023-10-03 11:37:32 -0300

    feat: bind peanut & money counters

--- src/app/peanuts/peanuts.component.ts
+++ src/app/peanuts/peanuts.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeanutsService } from './peanuts.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-peanuts',
   templateUrl: './peanuts.component.html',
   styleUrls: ['./peanuts.component.css']
 })
-export class PeanutsComponent {
+export class PeanutsComponent implements OnInit {
+  totalPeanutsSold$?: Observable<number>;
+  totalMoney$?: Observable<number>;
 
+  constructor(
+    private service: PeanutsService
+  ) { }
+
+  ngOnInit(): void {
+    this.totalPeanutsSold$ = this.service.totalProducedPeanuts$.pipe();
+    this.totalMoney$ = this.service.money$.pipe();
+  }
 }
