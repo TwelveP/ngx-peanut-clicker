c9b067a0361286e718676b6a43fb6b86ebcedde8
12p! <got12g@gmail.com>
2023-10-03 11:37:33 -0300

    test(class): `FinanceService`

--- src/app/finance.service.spec.ts
+++ src/app/finance.service.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import { concat, of } from 'rxjs';
+import { ignoreElements, take, takeLast, tap } from 'rxjs/operators';
 import { FinanceService } from './finance.service';
-import { of } from 'rxjs';
-import { SettingsService } from './settings.service';
+import { DEFAULT_SETTINGS, SettingsService } from './settings.service';
 
 describe('FinanceService', () => {
   let service: FinanceService;
@@ -9,12 +10,13 @@ describe('FinanceService', () => {
 
   beforeEach(() => {
     mockSettingService = {
-      settings$: of({
-        initialMoney: 0,
-        initialPeanuts: 0
-      })
+      settings$: of(DEFAULT_SETTINGS)
     };
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: SettingsService, useValue: mockSettingService }
+      ]
+    });
     service = TestBed.inject(FinanceService);
   });
 
@@ -22,5 +24,34 @@ describe('FinanceService', () => {
     expect(service).toBeTruthy();
   });
 
-  // TODO write some tests!
+  it('should hold default money balance', () => {
+    service.money$.pipe(
+      take(1),
+      tap(money => expect(money).toEqual(DEFAULT_SETTINGS.initialMoney))
+    ).subscribe();
+  });
+
+  it('should update money balance', () => {
+    /** from -50 to +50 */
+    const transactedAmount = (Math.random() * 100) - 50 || 1;
+    /** from 1 to 10 */
+    const times = (Math.random() * 9) + 1;
+    let initialMoney: number;
+
+    concat(
+      service.money$.pipe(
+        take(1),
+        tap(balance => (initialMoney = balance)),
+        ignoreElements()
+      ),
+      service.money$.pipe(
+        tap(balance => expect(balance).toBeDefined()),
+        tap(() => service.transact(transactedAmount)), // re-trigger observable emission
+        take(times)
+      )
+    ).pipe(
+      takeLast(1),
+      tap(balance => expect(balance).toBe(initialMoney + (times * transactedAmount)))
+    ).subscribe();
+  });
 });
