9f3f06c69baa6dd7dd3f0b9aeae87e407774dbb3
12p! <got12g@gmail.com>
2024-01-03 00:04:53 -0300

    feat: `simpleName` for `PricedElement`s

--- src/domain/base.ts
+++ src/domain/base.ts
@@ -1,4 +1,5 @@
 
 export interface PricedElement {
+    simpleName: string;
     initialProductionCost: number;
 }
--- src/domain/peanuts.ts
+++ src/domain/peanuts.ts
@@ -1,11 +1,13 @@
 import { PricedElement } from "./base";
 
 export class BagOfPeanuts implements PeanutProduct {
+    readonly simpleName = 'Bag of peanuts';
     readonly peanutsAmount = 15;
     readonly initialProductionCost = 50;
 }
 
 export class SinglePeanut implements PeanutProduct {
+    readonly simpleName = 'A peanut';
     readonly peanutsAmount = 1;
     readonly initialProductionCost = 3;
 }
